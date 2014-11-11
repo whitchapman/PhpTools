37ddf63efc6e8bd8708f7aac9c41e1daea76340a
Whit Chapman <whit.chapman@gmail.com>
2014-11-11 14:26:25 -0500

    refactored curl wrapper

--- library/lib/curl.php
+++ library/lib/curl.php
@@ -14,6 +14,9 @@ function log_curl($text, $type="MSG") {
 
 class CurlWrapper {
 	private $ch;
+	private $http_code;
+
+	public function get_http_code() { return $this->http_code; }
 
 	public function __construct() {
 		$this->ch = curl_init();
@@ -29,16 +32,42 @@ class CurlWrapper {
 		curl_setopt($this->ch, CURLOPT_HTTPHEADER, array($header));
 	}
 
-	public function query_url($url) {
-		log_curl("URL=".$url);
+	public function query($url) {
+
 		curl_setopt($this->ch, CURLOPT_URL, $url);
 		$response = curl_exec($this->ch);
-		return $response;
+		$this->http_code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+
+		if ($this->http_code == 200) {
+			log_curl("URL=".$url, "QRY");
+			//log_curl("HTTP_CODE=".$this->http_code, "AOK");
+			return $response;
+		}
+
+		if ($this->http_code == 404) {
+			log_curl("URL=".$url, "ERR");
+			log_curl("HTTP_CODE=".$this->http_code, "ERR");
+		} else {
+			log_curl("URL=".$url, "WRN");
+			log_curl("HTTP_CODE=".$this->http_code, "WRN");
+		}
+
+		return false;
 	}
 
-	public function get_http_code() {
-		$http_code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
-		return $http_code;
+	public function query_json($url) {
+		if ($response = $this->query($url)) {
+
+			$json = json_decode($response);
+			if (isset($json)) {
+				return $json;
+			}
+
+			log_curl("Failed to parse JSON (below); error #".json_last_error().": ".json_last_error_msg());
+			log_curl($response);
+		}
+
+		return false;
 	}
 }
 
